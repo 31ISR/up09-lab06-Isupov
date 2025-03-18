a951acfb593f4ec33d514e63d527ce46e6579cd2
Kostya Isupov <zzz.zz.z.zz1232176@gmail.com>
2025-03-18 10:23:45 +0000

    refactor(Controllers, resources): upgrade Todo and Note controllers, upgrade note Change the index method in NoteController public function index() {      = Note::query()->orderBy('created_at', 'desc')->paginate();     dd();     return view('note.index', ['notes' => ]); }
    Add content work to show and edit
    public function ...(Note )
    {
        return view('note.???', ['note' => ]);
    }
    
    Dispose of data from the DB in note/index.blade.php

--- note-app/app/Http/Controllers/NoteController.php
+++ note-app/app/Http/Controllers/NoteController.php
@@ -12,9 +12,12 @@ class NoteController extends Controller
      */
     public function index()
     {
-        return view('note.index');
+        $notes = Note::query()->orderBy('created_at', 'desc')->paginate();
+        dd($notes);
+        return view('note.index', ['notes' => $notes]);
     }
-
+    
+    
     /**
      * Show the form for creating a new resource.
      */
@@ -36,7 +39,7 @@ class NoteController extends Controller
      */
     public function show(Note $note)
     {
-        return view('note.show');
+        return view('note.show', ['note' => $note]);
     }
 
     /**
@@ -44,7 +47,7 @@ class NoteController extends Controller
      */
     public function edit(Note $note)
     {
-        return view('note.edit');
+        return view('note.edit', ['note' => $note]);
     }
 
     /**
